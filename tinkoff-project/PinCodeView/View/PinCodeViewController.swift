2401cbf2f7a8d670b396c58ef959ad5be10c2994
Viter <mrs_d_v@icloud.com>
2020-02-24 23:59:21 +0300

    Логика обработки пин-кода

--- tinkoff-project/PinCodeView/View/PinCodeViewController.swift
+++ tinkoff-project/PinCodeView/View/PinCodeViewController.swift
@@ -8,27 +8,119 @@
 
 import UIKit
 
+protocol PinCodeViewModelPrototcol: class {
+
+	var isHasAPinCode: Bool { get }
+
+	func setNumInPinCode(_ num: String)
+	func checkPinCodeStatus(_ senderName: String)
+
+	func deleteLastNumberInPinCode()
+	func cancel()
+}
+
 class PinCodeViewController: UIViewController {
 
-	@IBOutlet var numButtons: [UIButton]!
+	var isFirstPinCodeEnter = true  {
+		didSet {
+			enterPinLabel.text = "Подтвердите пин-код"
+		}
+	}
+	var pinCodeNumbers: String = "" {
+		didSet {
+			enteredPinCodeLabel.text? = pinCodeNumbers
+		}
+	}
 
+	private var model: PinCodeViewModelPrototcol!
+
+	// MARK: - IBOutlet
+	@IBOutlet var numButtons: [UIButton]!
+	@IBOutlet weak var enterPinLabel: UILabel!
+	@IBOutlet weak var doneOrSingInButton: UIButton!
+	@IBOutlet weak var enteredPinCodeLabel: UILabel!
+
+	// MARK: - LifeCicle
 	override func viewDidLoad() {
-        super.viewDidLoad()
+		super.viewDidLoad()
+
+		model = PinCodeViweModel(view: self)
+
+		setupViews()
+	}
+
+	// MARK: - Actions
+
+	@IBAction func tapNumButton(_ sender: UIButton) {
+
+		guard let number = sender.titleLabel?.text else { return }
+		model.setNumInPinCode(number)
+	}
+
+	@IBAction func tapSingInButton(_ sender: Any) {
+
+		guard let buttonLabel = doneOrSingInButton.titleLabel?.text else {
+			return }
+		model.checkPinCodeStatus(buttonLabel)
+	}
+
+	@IBAction func tapDeleteButton(_ sender: Any) {
+		model.deleteLastNumberInPinCode()
+	}
+
+	@IBAction func tapCancelButton(_ sender: Any) {
+		model.cancel()
+	}
+}
+
+// MARK: - Private
+extension PinCodeViewController {
+
+	private func setupViews() {
+		doneOrSingInButton.setTitle(model.isHasAPinCode ? "Войти" : "Done", for: .normal)
+		enteredPinCodeLabel.text = ""
 
 		numButtons.forEach { view in
 			self.drawCircle(rect: view.frame, view: view)
 		}
-		print("end")
-    }
-}
-
-extension PinCodeViewController {
-
+	}
 	private func drawCircle(rect: CGRect, view: UIButton) {
 
 		view.layer.cornerRadius = 25
 		view.layer.borderWidth = 1
 		view.layer.borderColor = UIColor.blue.cgColor
+	}
+}
 
+extension PinCodeViewController: PinCodeViewProtocol {
+
+	func changeSingInButton() {
+		doneOrSingInButton.setTitle("Войти", for: .normal)
+	}
+
+	func makeDoneButtonActive() {
+		guard let buttonText = doneOrSingInButton.titleLabel?.text,
+		buttonText == "Done"
+		else { return }
+	}
+
+	func shakeButtons() {
+		numButtons.forEach { view in
+			let animationKeyPath = "transform.translation.x"
+			let shakeAnimation = "shake"
+			let duration = 0.6
+			let animation = CAKeyframeAnimation(keyPath: animationKeyPath)
+			animation.timingFunction = CAMediaTimingFunction(name: .linear)
+			animation.duration = duration
+			animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0]
+			view.layer.add(animation, forKey: shakeAnimation)
+		}
+	}
+
+	func presentEmptyView() {
+		
+		let viewController = UIViewController()
+		viewController.view.backgroundColor = .lightText 
+		navigationController?.pushViewController(viewController, animated: true)
 	}
 }
--- tinkoff-project/PinCodeView/ViewModel/PinCodeViewModel.swift
+++ tinkoff-project/PinCodeView/ViewModel/PinCodeViewModel.swift
@@ -7,3 +7,104 @@
 //
 
 import Foundation
+
+protocol PinCodeViewProtocol: class {
+
+	var pinCodeNumbers: String { get set }
+	var isFirstPinCodeEnter: Bool { get set }
+
+	func makeDoneButtonActive()
+	func shakeButtons()
+	func presentEmptyView()
+	func changeSingInButton()
+}
+
+class PinCodeViweModel {
+
+	var isHasAPinCode: Bool
+	var tempPinCode: String = ""
+	private weak var view: PinCodeViewProtocol?
+	private var pinCode: String
+	private var userDefKey = "isHasPinCode"
+
+
+	init(view: PinCodeViewProtocol) {
+		self.isHasAPinCode = false
+		self.pinCode = ""
+		self.view = view
+	}
+}
+
+//MARK: - Private
+extension PinCodeViweModel {
+
+	private func check() -> Bool {
+
+		if tempPinCode == self.pinCode {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	private func userCanSingIn() {
+
+		if isHasAPinCode {
+			if check() {
+				clear()
+				view?.presentEmptyView()
+			} else {
+				clear()
+				view?.shakeButtons()
+			}
+		}
+	}
+
+	private func clear() {
+		view?.pinCodeNumbers = ""
+		tempPinCode = ""
+	}
+
+	private func setPinCode() {
+
+		isHasAPinCode = true
+		pinCode = tempPinCode
+		tempPinCode = ""
+
+		view?.pinCodeNumbers = ""
+		view?.changeSingInButton()
+
+		print(self.pinCode)
+
+		UserDefaults.standard.set(true, forKey: userDefKey)
+	}
+
+}
+
+extension PinCodeViweModel: PinCodeViewModelPrototcol {
+
+	func setNumInPinCode(_ num: String) {
+		tempPinCode += num
+		view?.pinCodeNumbers += "*"
+	}
+
+	func checkPinCodeStatus(_ senderName: String) {
+		if senderName == "Войти" {
+			userCanSingIn()
+		} else if senderName == "Done" && view?.isFirstPinCodeEnter ?? false {
+			setPinCode()
+			view?.isFirstPinCodeEnter = false
+		}
+	}
+
+	func deleteLastNumberInPinCode() {
+		tempPinCode.removeLast()
+		view?.pinCodeNumbers.removeLast()
+	}
+
+	func cancel() {
+		view?.pinCodeNumbers = ""
+		// TODO: return to main view
+	}
+
+}
