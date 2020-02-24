87ffd5e998f8cbb9ea00f7e65656df63c024ebe9
Zagid <aig.ukmu@gmail.com>
2020-02-24 15:57:53 +0300

    Create project

---
+++ tinkoff-project/LoginScreenModel.swift
@@ -0,0 +1,11 @@
+//
+//  LoginScreenModel.swift
+//  tinkoff-project
+//
+//  Created by Загид Гусейнов on 23.02.2020.
+//  Copyright © 2020 Загид Гусейнов. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ tinkoff-project/View/LoginScreenViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginScreenViewController.swift
+//  tinkoff-project
+//
+//  Created by Загид Гусейнов on 24.02.2020.
+//  Copyright © 2020 Загид Гусейнов. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+    
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+    @IBAction func pinCodeSwitch(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
