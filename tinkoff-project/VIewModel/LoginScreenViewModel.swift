dc97ad476ff197d24e550f44627f88755806cb78
Viter <mrs_d_v@icloud.com>
2020-02-24 17:53:50 +0300

    Дополнены LoginScreenViewModel, LoginScreenViewController

--- tinkoff-project/VIewModel/LoginScreenViewModel.swift
+++ tinkoff-project/VIewModel/LoginScreenViewModel.swift
@@ -8,6 +8,33 @@
 
 import Foundation
 
-class LoginScreenViewModel {
-    
+class LoginScreenViewModel: LoginScreenViewModelProtocol {
+
+	var login: String
+	var password: String
+	var isNeedPinCode: Bool
+
+	init(login: String, password: String, isNeedPinCode: Bool = false) {
+		self.login = login
+		self.password = password
+		self.isNeedPinCode = isNeedPinCode
+	}
+
+	func changeLogin(_ newLogin: String) {
+
+		if newLogin.count >= 3 && newLogin.count <= 20 {
+			self.login = newLogin
+		}
+	}
+
+	func changePassword(_ newPassword: String) {
+		if newPassword.count >= 8 && newPassword.count <= 20 {
+			self.password = newPassword
+		}
+	}
+
+	func setNeedPinCode(_ need: Bool) {
+		self.isNeedPinCode = need
+	}
+
 }
--- tinkoff-project/View/LoginScreenViewController.swift
+++ tinkoff-project/View/LoginScreenViewController.swift
@@ -8,19 +8,51 @@
 
 import UIKit
 
+protocol LoginScreenViewModelProtocol: class {
+	var login: String { get }
+	var password: String { get }
+	var isNeedPinCode: Bool { get }
+
+	func changeLogin(_ newLogin: String)
+	func changePassword(_ newPassword: String)
+	func setNeedPinCode(_ need: Bool)
+}
+
 class LoginScreenViewController: UIViewController {
-    
-    
+
+	var viewModel: LoginScreenViewModelProtocol!
+
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var enterButton: UIButton!
     @IBAction func pinCodeSwitch(_ sender: Any) {
-        
+
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+		viewModel = LoginScreenViewModel(login: "", password: "")
     }
+
+	@IBAction func tapOnScreen(_ sender: Any) {
+		resignFirstResponder()
+	}
+
+}
+
+extension LoginScreenViewController: UITextFieldDelegate {
+	func textFieldDidEndEditing(_ textField: UITextField) {
+		guard let text = textField.text else {
+			return
+		}
+		switch textField {
+		case loginTextField:
+			viewModel.changeLogin(text)
+		case passwordTextField:
+			viewModel.changePassword(text)
+		default:
+			break
+		}
+	}
 }
